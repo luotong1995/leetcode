e94a89cadc0cdbc76931b916271bdd34ff0958af
luotong1995 <954926501@qq.com>
2022-11-09 14:47:55 +0800

    train on leetcode

--- python/subset.py
+++ python/subset.py
@@ -8,9 +8,17 @@ def dfs(n, l, result, tl):
     dfs(n + 1, l, result, tl)
 
 
+def ac(i, nums, tmp, res):
+    res.append(tmp)
+    for j in range(i, len(nums)):
+        ac(j + 1, nums, tmp + [nums[j]], res)
+
 if __name__ == '__main__':
     a = [1, 2, 3, 4]
     res = []
     tl = []
     dfs(0, a, res, tl)
+    print(res)
+    res = []
+    ac(0, a, [], res)
     print(res)
